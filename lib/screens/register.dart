557c875d1682f93394d78b3bad0f6dc1cb427425
adrian.larry <adrian.larry@ui.ac.id>
2021-12-28 18:18:34 +0700

    fix pipeline error (?)

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import '../cookie_request.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
